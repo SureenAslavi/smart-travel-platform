d5c72973cf89fe52a20eb293d3b1c7f7e03f1de0
SureenAslavi <s2221189157@ku.edu.kw>
2026-05-03 01:28:41 +0300

    edit email

--- server.js
+++ server.js
@@ -80,7 +80,9 @@ function getAvailableSlots(date) {
 
 // ===== EMAIL SETUP =====
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587, 
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
